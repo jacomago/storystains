7967260f876928e7499ef08f464c6afeb39621d7
Sky Brewer <jaco.brewer@gmail.com>
2022-04-01 16:03:42 +0200

    split up code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use storystains::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    run().await
 }
