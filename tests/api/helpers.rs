77ad01f6e7255d3575fe151bdf98f842d9999fd4
Sky Brewer <jaco.brewer@gmail.com>
2022-04-04 11:22:48 +0200

    test running with config and db pool

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,12 +1,23 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
+use storystains::{configuration::get_configuration, startup::run};
 
-use storystains::startup::run;
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
 
-pub fn spawn_app() -> String {
+pub async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     // We return the application address to the caller!
     format!("http://127.0.0.1:{}", port)
