5d952b2f90a82cb1591331aaecc227e590c0812c
Sky Brewer <jaco.brewer@gmail.com>
2022-04-04 09:48:47 +0200

    get config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,11 @@
+use config::Config;
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application_port: u16,
 }
+
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
@@ -10,4 +13,14 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialise our configuration reader
+
+    let mut builder = Config::builder().add_source(config::File::with_name("configuration"));
+    match builder.build() {
+        Ok(config) => Ok(config.try_deserialize().unwrap()),
+        Err(e) => Err(e),
+    }
 }
