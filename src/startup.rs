0feb8ce874da51be1d375f45bcb7665732401614
Sky Brewer <jaco.brewer@gmail.com>
2022-04-04 18:00:09 +0200

    fmt

--- src/startup.rs
+++ src/startup.rs
@@ -51,10 +51,7 @@ impl Application {
     }
 }
 
-pub fn run(
-    listener: TcpListener,
-    db_pool: PgPool,
-) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
