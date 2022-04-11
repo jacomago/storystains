710ccebea90ad3b1f8f828fb7ab0085f2b1b06be
Sky Brewer <jaco.brewer@gmail.com>
2022-04-11 18:11:59 +0200

    test query parameter exists

--- src/catalog_client.rs
+++ src/catalog_client.rs
@@ -17,7 +17,7 @@ impl CatalogClient {
         }
     }
 
-    pub async fn get_book_url(&self, title: ReviewTitle) -> Result<(), reqwest::Error> {
+    pub async fn get_book_key(&self, title: ReviewTitle) -> Result<(), reqwest::Error> {
         let url = format!("{}/search.json", self.base_url);
         let query = BookQueryRequest {
             title: title.as_ref().to_owned(),
@@ -34,12 +34,27 @@ struct BookQueryRequest {
 
 #[cfg(test)]
 mod tests {
-    use wiremock::matchers::{path, method};
-    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use std::borrow::Cow;
+
+    use wiremock::matchers::{method, path};
+    use wiremock::{Mock, MockServer, Request, ResponseTemplate};
 
     use crate::domain::ReviewTitle;
 
     use super::CatalogClient;
+
+    struct GetBookKeyMatcher;
+
+    impl wiremock::Match for GetBookKeyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let mut result = request.url.query_pairs();
+            if let Some(entry) = result.next() {
+                entry.0 == Cow::Borrowed("title")
+            } else {
+                return false;
+            }
+        }
+    }
 
     #[tokio::test]
     async fn get_book_url_fires_a_request_to_base_url() {
@@ -49,6 +64,7 @@ mod tests {
 
         Mock::given(path("/search.json"))
             .and(method("GET"))
+            .and(GetBookKeyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
@@ -57,7 +73,7 @@ mod tests {
         let review_title = ReviewTitle::parse(format!("Lord of the Rings")).unwrap();
 
         //Act
-        let _ = catalog_client.get_book_url(review_title).await;
+        let _ = catalog_client.get_book_key(review_title).await;
 
         //Assert
     }
