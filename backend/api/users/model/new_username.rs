b92084806b72dbe5a3caeb4393815ff0cf42678a
Sky Brewer <jaco.brewer@gmail.com>
2022-06-06 11:50:13 +0200

    username not name

--- backend/api/users/model/new_username.rs
+++ backend/api/users/model/new_username.rs
@@ -28,7 +28,7 @@ impl NewUsername {
         let is_too_long = s.graphemes(true).count() > 256;
         // Iterate over all characters in the input `s` to check if any of them matches
         // one of the characters in the forbidden array.
-        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
@@ -65,14 +65,14 @@ mod tests {
     }
     #[test]
     fn names_containing_an_invalid_character_are_rejected() {
-        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}', ' '] {
             let name = name.to_string();
             assert_err!(NewUsername::parse(name));
         }
     }
     #[test]
     fn a_valid_name_is_parsed_successfully() {
-        let name = "Ursula Le Guin".to_string();
+        let name = "UrsulaLeGuin".to_string();
         assert_ok!(NewUsername::parse(name));
     }
 }
