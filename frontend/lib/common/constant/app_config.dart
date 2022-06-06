f31e7fee5553d98618778796ae24296561cb4b17
Sky Brewer <jaco.brewer@gmail.com>
2022-06-06 18:10:00 +0200

    use static folder for local web testing

--- frontend/lib/common/constant/app_config.dart
+++ frontend/lib/common/constant/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  static const baseUrl =
-      String.fromEnvironment("BASE_URL", defaultValue: "http://127.0.0.1:8080");
+  static const baseUrl = String.fromEnvironment("BASE_URL",
+      defaultValue: "http://127.0.0.1:8080/api");
   static const defaultLimit =
       int.fromEnvironment("defaultLimit", defaultValue: 20);
 }
