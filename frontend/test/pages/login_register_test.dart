42cd340cbe19322db7d7069a92d75e146f4cee8b
Sky Brewer <jaco.brewer@gmail.com>
2022-06-09 13:14:14 +0200

    login page test start

---
+++ frontend/test/pages/login_register_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:storystains/features/auth/auth.dart';
+import 'package:storystains/pages/login_register.dart';
+
+Widget wrapWithMaterial(Widget w) => MaterialApp(
+      home: ChangeNotifierProvider<AuthState>(
+        create: (_) => AuthState(AuthService()),
+        child: Scaffold(
+          body: w,
+        ),
+      ),
+    );
+
+
+void main() {
+  setUp(() => {WidgetsFlutterBinding.ensureInitialized()});
+  group("login", () {
+    testWidgets('After logging in isAuthenticated', (tester) async {
+      SharedPreferences.setMockInitialValues({});
+      await tester.pumpWidget(wrapWithMaterial(LoginOrRegisterPage()));
+      final passField = find.bySemanticsLabel('Password');
+      expect(passField, findsOneWidget);
+    });
+  });
+}
+
+// TODO change register and login
+// TODO fill in details
+// TODO login
+// TODO see failure message
+// TODO register
+// TODO see failure message
