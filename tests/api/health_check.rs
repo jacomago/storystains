cca9d633b828df5201362439ffd11c776405ad00
Sky Brewer <jaco.brewer@gmail.com>
2022-06-12 19:17:27 +0200

    test if emotion data incorrect

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -37,3 +37,27 @@ async fn db_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn db_check_fails_if_emotion_data_doesnt_match() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let _ =
+        sqlx::query!("INSERT INTO emotions(id, name, description) values (22, 'French', 'fab')")
+            .execute(&app.db_pool)
+            .await
+            .expect("Failed to fetch saved subscription.");
+    let response = client
+        // Use the returned application address
+        .get(&format!("{}/db_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_server_error());
+    assert_eq!(Some(0), response.content_length());
+}
