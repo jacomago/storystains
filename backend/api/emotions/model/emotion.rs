875685eee0388db23990ba7df1f471ebb8291c9b
Sky Brewer <jaco.brewer@gmail.com>
2022-06-13 12:16:20 +0200

    descriptions for emotion

--- backend/api/emotions/model/emotion.rs
+++ backend/api/emotions/model/emotion.rs
@@ -1,33 +1,63 @@
 use serde::Serialize;
 use strum::IntoEnumIterator;
-use strum_macros::{Display, EnumIter, EnumString};
+use strum_macros::{Display, EnumIter, EnumProperty, EnumString};
 
-#[derive(Debug, PartialEq, Serialize, EnumIter, EnumString, Display, Clone, Copy)]
+//TODO refactor to be a combination of the first six emotions with different amounts
+#[derive(Debug, PartialEq, Serialize, EnumIter, EnumString, Display, Clone, Copy, EnumProperty)]
 pub enum Emotion {
+    #[strum(props(description = "Strong displeasure against a someone or something."))]
     Anger = 1,
+    #[strum(props(description = "An aversion to something distasteful."))]
     Disgust = 2,
+    #[strum(props(description = "To expect danger."))]
     Fear = 3,
+    #[strum(props(description = "Strong pleasure."))]
     Joy = 4,
+    #[strum(props(description = "Unhappiness."))]
     Sadness = 5,
+    #[strum(props(description = "Reaction to an unexpected event."))]
     Surprise = 6,
+    #[strum(props(description = "Combination of Anger and disgust."))]
     Outrage = 7,
+    #[strum(props(description = "Combination of Anger and Fear."))]
     Trapped = 8,
+    #[strum(props(description = "Combination of Anger and Joy."))]
     Cruelty = 9,
+    #[strum(props(description = "Combination of Anger and Sadness."))]
     Betrayl = 10,
-    #[strum(serialize = "What the?")]
+    #[strum(
+        serialize = "What the?",
+        props(description = "Combination of Anger and Surprise.")
+    )]
     WhatThe = 11,
+    #[strum(props(description = "Combination of Disgust and Fear."))]
     Horror = 12,
+    #[strum(props(description = "Combination of Disgust and Joy."))]
     Eww = 13,
-    #[strum(serialize = "Pain Empathy")]
+    #[strum(
+        serialize = "Pain Empathy",
+        props(description = "Combination of Disgust and Sadness.")
+    )]
     PainEmpathy = 14,
-    #[strum(serialize = "You ate it!")]
+    #[strum(
+        serialize = "You ate it!",
+        props(description = "Combination of Disgust and Surprise.")
+    )]
     YouAteIt = 15,
+    #[strum(props(description = "Combination of Fear and Joy."))]
     Desperation = 16,
+    #[strum(props(description = "Combination of Fear and Sadness."))]
     Devastation = 17,
+    #[strum(props(description = "Combination of Fear and Surprise."))]
     Spooked = 18,
-    #[strum(serialize = "Faint Hope")]
+    #[strum(
+        serialize = "Faint Hope",
+        props(description = "Combination of Joy and Sadness.")
+    )]
     FaintHope = 19,
+    #[strum(props(description = "Combination of Joy and Surprise."))]
     Amazement = 20,
+    #[strum(props(description = "Combination of Sadness and Surprise."))]
     Disappointment = 21,
 }
 
@@ -38,6 +68,8 @@ pub fn emotions() -> Vec<Emotion> {
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
+
+    use strum::EnumProperty;
 
     use crate::api::emotions::model::Emotion;
 
@@ -71,5 +103,22 @@ mod tests {
     fn mapping_with_question() {
         let s = Emotion::WhatThe;
         assert_eq!(s, Emotion::from_str("What the?").unwrap());
+    }
+
+    #[test]
+    fn description() {
+        let s = Emotion::Desperation;
+        assert_eq!(
+            s.get_str("description").unwrap(),
+            "Combination of Fear and Joy."
+        );
+    }
+    #[test]
+    fn description_with_serialize() {
+        let s = Emotion::WhatThe;
+        assert_eq!(
+            s.get_str("description").unwrap(),
+            "Combination of Anger and Surprise."
+        );
     }
 }
