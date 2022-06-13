00703ca3ff4803a8bdc868d954c1f95a3fb29b62
Sky Brewer <jaco.brewer@gmail.com>
2022-06-13 14:35:02 +0200

    refactor to split up for future tests

---
+++ tests/api/emotion/get_list.rs
@@ -0,0 +1,40 @@
+use reqwest::StatusCode;
+use serde_json::{json, Value};
+
+use crate::{helpers::TestApp, review::TestReview};
+
+impl TestApp {
+    pub async fn get_emotions(&self) -> reqwest::Response {
+        self.api_client
+            .get(&format!("{}/emotions", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+}
+
+#[tokio::test]
+async fn get_emotions_returns_list() {
+    // Arrange
+    let app = TestApp::spawn_app().await;
+
+    // Act
+    let response = app.get_emotions().await;
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let json_page = response.text().await.unwrap();
+    let json: Value = serde_json::from_str(&json_page).unwrap();
+
+    assert!(json["emotions"].is_array());
+
+    let emotions = json["emotions"].as_array().unwrap();
+    assert_eq!(emotions.len(), 21);
+    assert!(emotions
+        .iter()
+        .map(|x| x.as_str())
+        .any(|x| x == Some("Joy")));
+    app.teardown().await;
+}
