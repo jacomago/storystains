5ad8f4354e3345f7317d67615bf8fe537e0ba7fc
Sky Brewer <jaco.brewer@gmail.com>
2022-06-14 08:38:15 +0200

    pull out user_id

--- backend/api/users/model/mod.rs
+++ backend/api/users/model/mod.rs
@@ -4,3 +4,5 @@ mod new_password;
 pub use new_password::NewPassword;
 mod new_username;
 pub use new_username::NewUsername;
+mod user_id;
+pub use user_id::UserId;
--- backend/api/users/model/users.rs
+++ backend/api/users/model/users.rs
@@ -1,43 +1,4 @@
-use std::ops::Deref;
-
 use super::{NewPassword, NewUsername};
-
-/// Format of an id of a user to stay consistent across application
-/// with multiple Uuid implementations
-#[derive(Copy, Clone, Debug, serde::Serialize, PartialEq, Eq)]
-pub struct UserId(uuid::Uuid);
-
-impl UserId {
-    /// Generates a new UserId
-    pub fn new(uuid: uuid::Uuid) -> Self {
-        Self(uuid)
-    }
-}
-
-impl std::fmt::Display for UserId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
-impl Deref for UserId {
-    type Target = uuid::Uuid;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl From<UserId> for sqlx::types::Uuid {
-    fn from(value: UserId) -> Self {
-        sqlx::types::Uuid::from_u128(value.0.as_u128())
-    }
-}
-
-impl From<sqlx::types::Uuid> for UserId {
-    fn from(uuid: sqlx::types::Uuid) -> Self {
-        Self(uuid::Uuid::from_u128(uuid.as_u128()))
-    }
-}
 
 #[derive(Debug)]
 pub struct StoredUser {
