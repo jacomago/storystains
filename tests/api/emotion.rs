d07cf7ad56f7f012c1172770ea3c9ea433b41881
Sky Brewer <jaco.brewer@gmail.com>
2022-06-16 19:09:10 +0200

    test svgs exist

--- tests/api/emotion.rs
+++ tests/api/emotion.rs
@@ -1,3 +1,4 @@
+use futures_lite::{stream, StreamExt};
 use reqwest::StatusCode;
 use serde_json::Value;
 
@@ -7,6 +8,13 @@ impl TestApp {
     pub async fn get_emotions(&self) -> reqwest::Response {
         self.api_client
             .get(&format!("{}/emotions", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+    pub async fn get_image(&self, icon_url: &str) -> reqwest::Response {
+        self.api_client
+            .get(&format!("{}/emotions/{}", &self.address, icon_url))
             .send()
             .await
             .expect("Failed to execute request.")
@@ -35,5 +43,31 @@ async fn get_emotions_returns_list() {
         .iter()
         .map(|x| x.as_str())
         .any(|x| x == Some("Joy")));
+    app.teardown().await;
+}
+
+#[tokio::test]
+async fn get_emotion_image_returns_svg() {
+    // Arrange
+    let app = TestApp::spawn_app().await;
+
+    // Act
+    let response = app.get_emotions().await;
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let json_page = response.text().await.unwrap();
+    let json: Value = serde_json::from_str(&json_page).unwrap();
+
+    assert!(json["emotions"].is_array());
+
+    let emotions = json["emotions"].as_array().unwrap();
+    for emotion in emotions {
+        let response = app.get_image(emotion["icon_url"].as_str().unwrap()).await;
+        assert_eq!(response.status(), StatusCode::OK);
+        let content_type: &str = response.headers()["Content-Type"].to_str().unwrap();
+        assert_eq!(content_type, "SVG");
+    }
     app.teardown().await;
 }
