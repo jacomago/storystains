c10491eec325f3d70a67530eb0021959bebc1654
Sky Brewer <jaco.brewer@gmail.com>
2022-06-18 09:45:41 +0200

    fix not using configed base url

--- frontend/lib/common/utils/services.dart
+++ frontend/lib/common/utils/services.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:storystains/common/constant/app_config.dart';
 import 'package:storystains/common/data/network/dio_manager.dart';
 import 'package:storystains/common/data/network/rest_client.dart';
 
@@ -9,6 +10,8 @@ class Services {
   static void setup() {
     final dioManager = DioManager();
     sl.registerSingleton<DioManager>(dioManager);
-    sl.registerLazySingleton<RestClient>(() => RestClient(dioManager.dio));
+    sl.registerLazySingleton<RestClient>(
+      () => RestClient(dioManager.dio, baseUrl: AppConfig.baseUrl),
+    );
   }
 }
