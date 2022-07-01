237e6a9905c83271faa407adc91c8a6043fe86a1
Sky Brewer <jaco.brewer@gmail.com>
2022-07-01 08:21:19 +0200

    test account logout

--- frontend/test/pages/account_test.dart
+++ frontend/test/pages/account_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:storystains/common/utils/service_locator.dart';
 import 'package:storystains/features/auth/auth.dart';
 import 'package:storystains/pages/account.dart';
 
@@ -56,6 +57,33 @@ void main() {
 
       expect(
         find.widgetWithText(SnackBar, "Delete user failed"),
+        findsOneWidget,
+      );
+    });
+    testWidgets('Account log out', (tester) async {
+      SharedPreferences.setMockInitialValues({});
+      ServiceLocator.setup();
+
+      final widg = wrapWithMaterial(
+        Builder(builder: (BuildContext context) {
+          return const AccountPage();
+        }),
+        AuthState(AuthService()),
+      );
+
+      await tester.pumpWidget(widg);
+
+      var button = find.widgetWithText(ElevatedButton, 'Log out');
+      expect(button, findsOneWidget);
+
+      await tester.ensureVisible(button);
+      await tester.tap(button.first);
+      await tester.pump();
+      await tester.pump();
+      await tester.pump();
+
+      expect(
+        find.widgetWithText(SnackBar, "Logout succeded"),
         findsOneWidget,
       );
     });
