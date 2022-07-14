8c12bfc854c0d55eb31419808a212a65e2a11291
Sky Brewer <jaco.brewer@gmail.com>
2022-07-14 17:11:21 +0200

    test doesn't work

--- frontend/test/features/story/story_widget_test.mocks.dart
+++ frontend/test/features/story/story_widget_test.mocks.dart
@@ -2,11 +2,12 @@
 // in storystains/test/features/story/story_widget_test.dart.
 // Do not manually edit this file.
 
-import 'dart:async' as _i3;
+import 'dart:async' as _i4;
 
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:storystains/features/mediums/medium_model.dart' as _i4;
-import 'package:storystains/features/mediums/medium_service.dart' as _i2;
+import 'package:storystains/features/mediums/medium_model.dart' as _i5;
+import 'package:storystains/features/mediums/medium_service.dart' as _i3;
+import 'package:storystains/features/story/story.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -18,17 +19,41 @@ import 'package:storystains/features/mediums/medium_service.dart' as _i2;
 // ignore_for_file: unnecessary_parenthesis
 // ignore_for_file: camel_case_types
 
+class _FakeWrappedStory_0 extends _i1.Fake implements _i2.WrappedStory {}
+
+class _FakeStoriesResp_1 extends _i1.Fake implements _i2.StoriesResp {}
+
 /// A class which mocks [MediumsService].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockMediumsService extends _i1.Mock implements _i2.MediumsService {
+class MockMediumsService extends _i1.Mock implements _i3.MediumsService {
   MockMediumsService() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i3.Future<List<_i4.Medium>?> fetch() =>
+  _i4.Future<List<_i5.Medium>?> fetch() =>
       (super.noSuchMethod(Invocation.method(#fetch, []),
-              returnValue: Future<List<_i4.Medium>?>.value())
-          as _i3.Future<List<_i4.Medium>?>);
+              returnValue: Future<List<_i5.Medium>?>.value())
+          as _i4.Future<List<_i5.Medium>?>);
+}
+
+/// A class which mocks [StoryService].
+///
+/// See the documentation for Mockito's code generation for more information.
+class MockStoryService extends _i1.Mock implements _i2.StoryService {
+  MockStoryService() {
+    _i1.throwOnMissingStub(this);
+  }
+
+  @override
+  _i4.Future<_i2.WrappedStory> create(_i2.Story? story) => (super.noSuchMethod(
+          Invocation.method(#create, [story]),
+          returnValue: Future<_i2.WrappedStory>.value(_FakeWrappedStory_0()))
+      as _i4.Future<_i2.WrappedStory>);
+  @override
+  _i4.Future<_i2.StoriesResp> search(_i2.Story? story) =>
+      (super.noSuchMethod(Invocation.method(#search, [story]),
+              returnValue: Future<_i2.StoriesResp>.value(_FakeStoriesResp_1()))
+          as _i4.Future<_i2.StoriesResp>);
 }
