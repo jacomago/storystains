f0fbb598ccdfbd50f061d032b271847b0a9dd251
Sky Brewer <jaco.brewer@gmail.com>
2022-07-25 14:19:54 +0200

    remove unused import

--- frontend/test/features/review_emotions/review_emotions_list_test.dart
+++ frontend/test/features/review_emotions/review_emotions_list_test.dart
@@ -2,7 +2,6 @@ import 'dart:io' as io;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
