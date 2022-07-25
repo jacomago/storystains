4e6f0e787fd9f28eb20eb11e73f268a6b29db360
Sky Brewer <jaco.brewer@gmail.com>
2022-07-25 14:50:57 +0200

    clean up hints

--- frontend/lib/features/story/widgets/story_widget.dart
+++ frontend/lib/features/story/widgets/story_widget.dart
@@ -10,24 +10,15 @@ class StoryWidget extends StatelessWidget {
   const StoryWidget({Key? key, required this.story}) : super(key: key);
 
   void _onTapTitle(BuildContext context) {
-    if (story?.title == null) {
-      return;
-    }
-    StoryQuery(title: story!.title).navigate(context);
+    StoryQuery(title: story.title).navigate(context);
   }
 
   void _onTapCreator(BuildContext context) {
-    if (story?.creator == null) {
-      return;
-    }
-    StoryQuery(creator: story!.creator).navigate(context);
+    StoryQuery(creator: story.creator).navigate(context);
   }
 
   void _onTapMedium(BuildContext context) {
-    if (story?.medium == null) {
-      return;
-    }
-    StoryQuery(medium: story!.medium).navigate(context);
+    StoryQuery(medium: story.medium).navigate(context);
   }
 
   ///The [Story] to display
@@ -42,14 +33,14 @@ class StoryWidget extends StatelessWidget {
             children: [
               LinkButton(
                 onPressed: () => _onTapTitle(context),
-                text: '${story?.title}',
+                text: story.title,
                 defaultStyle: context.headlineSmall!,
                 semanticsLabel: context.locale.title,
                 maxLines: 2,
               ),
               LinkButton(
                 onPressed: () => _onTapMedium(context),
-                text: '(${story?.medium.name})',
+                text: '(${story.medium.name})',
                 defaultStyle: context.titleMedium!,
                 semanticsLabel: context.locale.medium,
               ),
@@ -57,7 +48,7 @@ class StoryWidget extends StatelessWidget {
           ),
           LinkButton(
             onPressed: () => _onTapCreator(context),
-            text: context.locale.byCreator(story?.creator ?? ''),
+            text: context.locale.byCreator(story.creator),
             defaultStyle: context.titleMedium!.copyWith(
               fontStyle: FontStyle.italic,
             ),
