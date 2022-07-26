6f1a1492aa60cb715ca7d0adfcfb40b5ce61f108
Sky Brewer <jaco.brewer@gmail.com>
2022-07-26 10:10:53 +0200

    add a test for the bug

--- frontend/test/features/review/review.dart
+++ frontend/test/features/review/review.dart
@@ -1,4 +1,5 @@
 import 'package:storystains/features/review/review_model.dart';
+import 'package:storystains/features/review_emotion/review_emotion.dart';
 
 import '../auth/user.dart';
 import '../story/story.dart';
@@ -6,6 +7,7 @@ import '../story/story.dart';
 Review testReview({
   String? slug,
   String? username,
+  List<ReviewEmotion>? emotions,
 }) =>
     Review(
       body: 'body$slug',
@@ -13,6 +15,6 @@ Review testReview({
       slug: slug ?? 'title',
       story: testStory(title: slug),
       updatedAt: DateTime.now(),
-      emotions: [],
+      emotions: emotions ?? [],
       user: testUserProfile(username: username),
     );
