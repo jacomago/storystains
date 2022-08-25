788ecc62cc77c54dae726b84ecaeb896c3cf7aea
Sky Brewer <jaco.brewer@gmail.com>
2022-08-25 17:34:29 +0200

    svelte start

---
+++ website/src/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<header class="container mx-auto sm:flex sm:justify-between sm:max-w-3xl xl:max-w-5xl">
+	<div class="flex items-center justify-between px-4 py-2">
+		<div>
+			<a href="/">
+				<img class="h-8" src="/images/logos/icon-logo.svg" alt="StoryStains" />
+			</a>
+		</div>
+	</div>
+	<div class="px-1 py-3 pb-3 shadow-lg font-normal rounded sm:flex sm:shadow-none">
+		<a
+			class="block mt-1 text-center py-2 text-gray-900 hover:bg-blue-400 rounded text-l sm:mx-3 sm:hover:bg-transparent sm:hover:text-red-500 xl:text-xl"
+			href="/">Home</a
+		>
+		<a
+			class="block mt-1 text-center py-2 text-gray-900 hover:bg-blue-400 rounded text-l sm:mx-3 sm:hover:bg-transparent sm:hover:text-red-500 xl:text-xl"
+			href="/about">About</a
+		>
+	</div>
+</header>
---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navbar from '../components/Navbar.svelte';
+
+	import '../app.css';
+</script>
+
+<Navbar />
+
+<div class="flex-grow self-center mt-16 mb-16 px-4">
+	<slot />
+</div>
