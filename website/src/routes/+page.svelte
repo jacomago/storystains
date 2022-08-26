6efd02af22134c8e0c6f18d7103965c15384d943
Sky Brewer <jaco.brewer@gmail.com>
2022-08-26 15:06:44 +0200

    a bit of updated text

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-row flex-wrap md:flex-no-wrap items-center justify-center">
+	<div class="md:w-1/2 flex flex-col">
+		<h1 class="text-5xl text-center font-display pb-2 leading-tight">StoryStains</h1>
+		<p class="text-xl text-center font-normal">An emotion driven story analysis app.</p>
+	</div>
+</div>
+
+<section>
+	<div class="container mx-auto flex px-4 py-24 md:flex-row flex-col items-center">
+		<p>Show the emotions that affect you in your favourite stories.</p>
+	</div>
+</section>
