cf3c8d572ff2f1e87a1ca645c8b6bcb1a622d72b
Sky Brewer <jaco.brewer@gmail.com>
2022-08-26 15:24:54 +0200

    add an about

---
+++ website/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-row flex-wrap md:flex-no-wrap items-center justify-center">
+	<div class="md:w-1/2 flex flex-col">
+		<h1 class="font-display text-xl text-center">Hi</h1>
+		<p class="text-xl text-center font-normal">My name is Sky.</p>
+	</div>
+</div>
+
+<p>
+	I'm a software developer working on this app to help me understand fiction so I can write better
+	fiction myself.
+</p>
