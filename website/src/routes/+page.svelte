dca15a79f459588f1fe652299c3351cb165ee398
Sky Brewer <jaco.brewer@gmail.com>
2022-08-29 11:07:52 +0200

    store buttons and icon

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -2,7 +2,26 @@
 	<div class="md:w-1/2 flex flex-col">
 		<img src="/images/titletext.svg" alt="StoryStains" />
 		<p class="logline text-xl text-center font-normal">An emotion driven story analysis app.</p>
+
+		<div class="flex flex-col items-center justify-center mt-16 mb-8">
+			<a class="mx-1 my-2" href="/android">
+				<img
+					alt="Get it on Google Play"
+					src="/images/android-store-badge.png"
+					style="height: 60px;"
+				/>
+			</a>
+			<a class="mx-1 my-2" href="/apple">
+				<img
+					alt="Download on the App Store"
+					src="/images/ios-store-badge.svg"
+					style="height: 60px;"
+				/>
+			</a>
+		</div>
 	</div>
+
+	<img src="/images/newicontest1.png" alt="" class="pt-12 px-4 md:w-1/2 md:pt-16 md:px-16" />
 </div>
 
 <section>
