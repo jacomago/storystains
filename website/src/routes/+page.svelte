24990866b172c5c3629d0ada6dab64d38e21e12b
Sky Brewer <jaco.brewer@gmail.com>
2022-09-14 15:08:08 +0200

    update emotions to fountain pen style

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -26,13 +26,6 @@
 
 <section>
 	<div class="container mx-auto flex px-4 py-24 md:flex-row flex-col items-center">
-		<h3>Find the emotions of your stories.</h3>
-		<p>Storystains allows you to analyze stories by their emotions.</p>
-	</div>
-</section>
-
-<section>
-	<div class="container mx-auto flex px-4 py-24 md:flex-row flex-col items-center">
 		<h3>Breakdown stories into the important points.</h3>
 		<p>StoryStains allows you to set what triggers your emotions.</p>
 	</div>
