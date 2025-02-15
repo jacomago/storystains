a1e8bc3af6544b85dbfd2b6c5539158b8821ad12
Sky Brewer <sky.brewer@ess.eu>
2025-02-15 16:16:50 +0100

    Add wrangler and cloudflare settings

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -12,7 +12,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
 	},
 
 	extensions: ['.svelte', '.svx']
